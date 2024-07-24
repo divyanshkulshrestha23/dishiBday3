1f8528c4bfe46ba9bad7064c12fe4ccdeb4e10e4
divyanshkulshrestha23 <145012534+divyanshkulshrestha23@users.noreply.github.com>
2024-07-24 16:14:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, Response
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
