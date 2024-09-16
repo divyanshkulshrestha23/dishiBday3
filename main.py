9a273f9b6f7b3f39d5a5af6bed1700c708bc9fd6
divyanshkulshrestha23 <145012534+divyanshkulshrestha23@users.noreply.github.com>
2024-09-17 01:13:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,6 @@ from flask_migrate import Migrate
 import boto3
 import os
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
